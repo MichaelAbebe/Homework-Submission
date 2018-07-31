e7b45408d58dfc5354ac075bf09a726865d26927
MichaelAbebe <mikmikeluda@gmail.com>
2018-07-31 18:26:27 -0400

    javascript homework

---
+++ Week 5/homework.js
@@ -0,0 +1,71 @@
+// question number 1
+console.log("Hellow World!!")
+console.log("Hellow Alem!!")
+// question number 2
+// the error was the use of single quote when also using '
+cosole.log("I'm awesome")
+// question number 3
+var x;
+console.log("value of x is null")
+console.log(x)
+var x=100;
+console.log("value of x is 100")
+console.log(x)
+// question number 4
+var y="Mike";
+console.log("value of y is Mike")
+console.log(y)
+var y="Abebe";
+console.log("value of y is Abebe")
+console.log(y)
+// question number 5
+var z=7.25;
+console.log(z)
+var a=Math.round(z)
+console.log(a)
+
+  var x=a;
+  console.log(x)
+}
+else{
+  var x=z;
+  console.log(x)
+}
+// question number 6
+var names;
+console.log("Value of names is undefined")
+conslole.log(names)
+var myFavAnimals=["dog","lion","rabbit"];
+console.log(myFavAnimals)
+myFavAnimals.push("kitten");
+console.log(myFavAnimals)
+// question 7
+let myString="this is a test";
+var l=myString.length;
+console.log(l)
+// question 8
+var x=7;
+x=x%3;
+console.log(x)//1
+
+var x=8;
+x=x%3;
+console.log(x)//2
+
+var x=10;
+x=x%3;
+console.log(x)//1
+
+console.log("What % does is divides the number with the stated value and returns the remainder")
+// question: 9a
+Yes we can
+var array=["mike",2,"ithinkso",6,-12,"thinkso"];
+console.log(array)
+// quesstion 9b
+var x=6;
+var y=10;
+if (x/0===y/0){
+console.log("it compares it" );
+}
+// question 9c
+console.log("It compares it and i guess thats beacause even infinty is a value  ")
